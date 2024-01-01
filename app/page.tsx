41257e19d71e343abd846d4b97ef619c1d18e6ea
ahmaddzidnii <zdngaming312@gmail.com>
2024-01-01 22:32:15 +0700

    01:07:13

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,21 @@ export default function Home() {
   return (
     <main className="flex h-full flex-col justify-center items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-sky-600">
       <div className="space-y-6 text-center">
-        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", popins.className)}>🔐Auth</h1>
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            popins.className
+          )}
+        >
+          🔐Auth
+        </h1>
         <p className="text-white text-lg">A simple authentication service</p>
       </div>
       <LoginButton>
-        <Button variant="secondary" size="lg">
+        <Button
+          variant="secondary"
+          size="lg"
+        >
           SignIn
         </Button>
       </LoginButton>
